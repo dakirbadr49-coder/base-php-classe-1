781166050946bae1e7b5cabf213aee8929fc36b3
Formateurs Web <146428844+WebDevCF2m@users.noreply.github.com>
2026-03-18 09:38:17 +0100

    Refactor PHP file to include HTML and current date
    Thanks very much Yuliia

--- Stagiaire/Yuliia/05-php-placement.php
+++ Stagiaire/Yuliia/05-php-placement.php
@@ -1,6 +1,15 @@
 <?php
-// Générer la table de multiplication de 10
-for ($i = 1; $i <= 12; $i++) {
-   echo "10 x $i = " . (10 * $i). "\n";
-}
+  $date =  date("F j, Y, g:i a");
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ma page - <?= date("H:i") ?></title>
+</head>
+<body>
+    <h1>Date : <?= date("F j, Y") ?></h1>
+  <p>Il est <?= date("g:i a") ?></p>
+</body>
+</html>
